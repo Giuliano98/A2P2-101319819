3a267168206d958c72a1c88ae2a356a5a3399376
Giuliano Venturo <giulianoventuro1998@gmail.com>
2023-12-11 20:27:42 -0500

    debugging

---
+++ Assets/[Scripts]/Door.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    GameplayManager gameplayManager;
+    GameOver gameOver;
+
+    private void Start()
+    {
+        gameplayManager = FindObjectOfType<GameplayManager>();
+        gameOver = FindObjectOfType<GameOver>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Player"))
+            return;
+        if (gameplayManager.FoundKeys != gameplayManager.TotalKeys)
+            return;
+
+        gameOver.ShowGameOverUI();
+    }
+}
